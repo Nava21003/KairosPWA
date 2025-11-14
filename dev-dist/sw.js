b884e468d4b70392e5a6da23e9ed88fcc87b3f81
Nava21003 <angelmessijr92@gmail.com>
2025-11-13 19:34:29 -0600

    Commit APIs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l26j4u5qqpo"
+    "revision": "0.aoh4al70g2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
